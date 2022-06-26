113e836dfbcb02a6f229e3e4bcfb9847d5a7c6c7
m <murrayataga@gmail.com>
2022-06-26 14:52:32 +0100

    Add emailvalidation fixture

---
+++ cypress/integration/EmailValidation.cy.js
@@ -0,0 +1,13 @@
+///<reference types="cypress" />
+describe("Email validation Test Cases",function(){
+    before("Load email validation fixture",function(){
+        cy.fixture("emailVal").then(function(data){
+            
+
+        })
+    })
+
+    it("should validate that an email exists",function(){
+        
+    })
+})
