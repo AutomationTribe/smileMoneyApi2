8cf98fa3038313d58b80ae2c413dc5f499c7a0d9
m <murrayataga@gmail.com>
2022-06-28 15:05:35 +0100

    Add email validation

--- cypress/integration/EmailValidation.cy.js
+++ cypress/integration/EmailValidation.cy.js
@@ -1,13 +1,48 @@
 ///<reference types="cypress" />
-describe("Email validation Test Cases",function(){
-    before("Load email validation fixture",function(){
-        cy.fixture("emailVal").then(function(data){
-            
+describe("Email validation test cases",function(){
 
+    before("Retrieve or insert email to DB",function(){
+        let existingUserEmail;
+        cy.task("querDb","select * from smilemoney.agent").then((data)=>{
+            if(data.length > 0){
+                existingUserEmail = data[0].email;
+            }else{
+                
+                console.log("No agent found");
+            }
         })
     })
+   
+    it("should validate an existing email",function(){
 
-    it("should validate that an email exists",function(){
-        
+        cy.request({
+            "method" : "POST",
+            "url": "https://smilemoney-sandbox.renmoney.com/customer/email_validation",
+            "body" : {
+                "email":existingUserEmail,
+                "networkKey":"4342424"
+            }
+        }).then((response)=>{
+
+            expect(response.status).to.eq(200);
+        })
+
     })
+
+    it("should not validate a non-existing email",function(){
+
+        cy.request({
+            "method" : "POST",
+            "url": "https://smilemoney-sandbox.renmoney.com/customer/email_validation",
+            "body" : {
+                "email":"foo@bar.com",
+                "networkKey":"4342424"
+            }
+        }).then((response)=>{
+
+            expect(response.status).to.eq(404);
+        })
+
+    })
+
 })
