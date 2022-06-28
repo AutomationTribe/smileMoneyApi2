bdbba5ff62796c1ab1752587cf53fa02276244b9
m <murrayataga@gmail.com>
2022-06-28 15:04:31 +0100

    Add loan history tests

---
+++ cypress/integration/LoanHistory.cy.js
@@ -0,0 +1,46 @@
+///<reference types="cypress" />
+describe("Test cases for Loan History module",function(){
+    let token,phoneNumber;
+    before("Log user in",function(){
+       cy.authenticate().then((data)=>{
+        token = data;
+      });
+      cy.fixture('LoanHistory').then((fdata)=>{
+        phoneNumber = fdata;
+      })
+    })
+
+    it("should retrieve loan history of user with active loans",function(){
+        console.log(token);
+        cy.request({
+            method : "GET",
+            url : "https://smilemoney-sandbox.renmoney.com/loan/get_loans/07038729221",
+            failOnStatusCode:false,
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response)=>{
+            expect(response.status).to.eq(200);
+            expect(response.body.status).to.eq("successful");
+
+        })
+    })
+
+    it("should not retrieve loan history of user with no active loans",function(){
+        console.log(token);
+        cy.request({
+            method : "GET",
+            url : "https://smilemoney-sandbox.renmoney.com/loan/get_loans/07018729221",
+            failOnStatusCode:false,
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response)=>{
+            expect(response.status).to.eq(404);
+            expect(response.body.status).to.eq("failed");
+
+        })
+    })
+
+    
+})
